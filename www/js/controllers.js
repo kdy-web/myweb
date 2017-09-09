66e96fb8669ecfabb88b4fa76d588d1a9731371c
kdy-web <936931739@qq.com>
2017-09-09 17:29:08 +0800

    修改轮播图BUG

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -225,7 +225,7 @@ $timeout(function(){
   		
   	$scope.imgData=$scope.slideData[i].img
     console.log($scope.id)
-  	 $scope.swiper = new Swiper('.slide_swiper', {
+  	var swiper = new Swiper('.slide_swiper', {
   	 	
 	 	 initialSlide :0,
 	    observer:true,//修改swiper自己或子元素时，自动初始化swiper
