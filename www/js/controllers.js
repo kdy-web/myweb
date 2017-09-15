08ce7c670371e68752d6b41f41881d47be7f9507
kdy-web <936931739@qq.com>
2017-09-15 10:28:10 +0800

    cache

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -752,10 +752,9 @@ var message=JSON.stringify($scope.person)
 })
 .controller("AdviceCtrl",function($scope,$rootScope){
 	$scope.$on('$ionicView.beforeEnter',function(){
-		   if (location.href.indexOf("?xyz=")<0){
-		    	window.location.reload();//页面刷新一次
-			 location.href=location.href+"?xyz="+Math.random();
-			 }
+		 
+		    	$scope.doRefresh(); //页面刷新一次
+			
 
 	})
 		document.getElementById("textarea").focus()
